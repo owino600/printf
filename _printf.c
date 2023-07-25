65c0c47342617891b5d8bccfe35a2b0729d73ae5
owino600 <owinodaniel600@gmail.com>
2023-07-25 14:13:05 -0700

    _printf

--- _printf.c
+++ _printf.c
@@ -1,11 +1,19 @@
 #include "main.h"
+/**
+ * functions = _printf
+ * description: custom _printf function that print fommarted output
+ * @s: form strings and other arguments
+ * @c: number of characters pointed
+ * Return: the number of characters printed:wq
+ */
 
 void print_buffer(char buffer[], int *buff_ind);
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int count = 0;
-	int buff_ind = 0;
+	int count = 0; /*initilize a counter for the number of characters printed*/
+	int buff_ind = 0; /*Buffer idex*/
+	int printed = 0;
 	int ch = va_arg(args, int);
 	char *str = va_arg(args, char*);
 	char buffer[BUFF_SIZE];
@@ -13,14 +21,15 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	while (*format)
 	{
-		if (*format == '%')
+		if (*format == '%')/*check for the start of a conversion specifier*/
 		{
-			format++;
+			format++;/*move to the next character after '%'*/
 			if (*format == '%')
 			{
 				buffer[buff_ind++] = '%';
 				if (buff_ind == BUFF_SIZE)
 				{
+					/*flash the buffer and print its content*/
 					print_buffer(buffer, &buff_ind);
 					count += buff_ind;
 				}
@@ -45,25 +54,31 @@ int _printf(const char *format, ...)
 						print_buffer(buffer, &buff_ind);
 						count += buff_ind;
 					}
-					
 				}
 			}
-			else
+			else if (*format == 'd' || *format == 'i')
 			{
-				buffer[buff_ind++] = *format;
-				if (buff_ind == BUFF_SIZE)
-				{
-					print_buffer(buffer, &buff_ind);
-					count += buff_ind;
-				}
+				int value = va_arg(args, int);
+				printed = print_integer(value);
+				count += printed;
 			}
-			format++;
 		}
-		print_buffer(buffer, &buff_ind);
-		count += buff_ind;
-
-		va_end(args);
+		else
+		{
+			buffer[buff_ind++] = *format;
+			if (buff_ind == BUFF_SIZE)
+			{
+				print_buffer(buffer, &buff_ind);
+				count += buff_ind;
+			}
+		}
+		format++;
 	}
+	print_buffer(buffer, &buff_ind);
+	count += buff_ind;
+
+	va_end(args);
+
 	return (count);
 }
 int print_integer(int value)
